6b263c65d15ec3656c4f424f2ab9c1cf2e20e79d
Dmitry <babanov91@gmail.com>
2023-10-14 19:35:38 +0500

    remove duplicate

--- leetcode.py
+++ leetcode.py
@@ -16,7 +16,7 @@ def twoSum(nums: List[int], target: int) -> List[int]:
 
 nums = [2, 7, 15, 17]
 target = 9
-print(twoSum(nums, target))
+# print(twoSum(nums, target))
 
 
 #9 Palindrome Number
@@ -69,4 +69,90 @@ def longestCommonPrefix(strs: List[str]) -> str:
             break
     return ans
 
-print(longestCommonPrefix(["flower","flow","flight"]))
+# print(longestCommonPrefix(["flower","flow","flight"]))
+
+# 20. Valid Parentheses
+
+"""Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type."""
+
+def isValid(s: str) -> bool:
+    stack = []
+    brackets = {'(': ')', '{': '}', '[': ']'}
+
+    for c in s:
+        if c in brackets:
+            # print(c)
+            stack.append(c)
+            # print(stack)
+        elif stack and c == brackets[stack.pop()]:
+            continue
+        else:
+            return False
+    print(stack)
+    return not stack
+
+# print(isValid('()[]{}'))
+
+# 21. Merge Two Sorted Lists
+'''You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.'''
+
+
+'''Definition for singly-linked list.'''
+
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    # def __str__(self):
+    #     # return "dafsdf"
+    #     return str(self.val)
+
+def mergeTwoLists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    # Create a dummy node
+    dummy = ListNode(0)
+    cur = dummy
+
+    while l1 and l2:
+        if l1.val <= l2.val:
+            cur.next, l1 = l1, l1.next
+        else:
+            cur.next, l2 = l2, l2.next
+
+        cur = cur.next
+
+    # In case any has still nodes left, just append them
+    cur.next = l1 if l1 else l2
+
+    return dummy.next
+
+list1 = ListNode([1,4,5,5,9])
+list2 = ListNode([1,3,6,8,8])
+
+# print(mergeTwoLists(list1,list2))
+# mergeTwoLists(list1,list2)
+
+
+# 26. Remove Duplicates from Sorted Array
+"""Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k."""
+from collections import OrderedDict
+
+def removeDuplicates(nums: List[int]) -> int:
+    # nums[:] =  OrderedDict.fromkeys(nums)
+    # return len(nums)
+    nums[:] = sorted(set(nums))
+    return len(nums)
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+print(removeDuplicates(nums))
+print(removeDuplicates([1,1,2]))
