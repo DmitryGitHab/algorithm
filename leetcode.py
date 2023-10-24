61de950fa21f09e8cfb29dd02b219f38b1d2f63b
Dmitry <babanov91@gmail.com>
2023-10-24 23:44:00 +0500

    remove element

--- leetcode.py
+++ leetcode.py
@@ -154,5 +154,20 @@ def removeDuplicates(nums: List[int]) -> int:
     return len(nums)
 
 nums = [0,0,1,1,1,2,2,3,3,4]
-print(removeDuplicates(nums))
-print(removeDuplicates([1,1,2]))
+# print(removeDuplicates(nums))
+# print(removeDuplicates([1,1,2]))
+
+# 27. Remove Element
+'''Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.'''
+
+def removeElement(nums: List[int], val: int) -> int:
+    nums[:] = [num for num in nums if num != val]
+
+    return len(nums), nums
+
+nums = [3,2,2,3]
+val = 3
+print(removeElement(nums, val))
